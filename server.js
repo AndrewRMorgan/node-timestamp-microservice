fef67a8b463027f64a70295cab145f178bbe6053
AndrewRMorgan <andrew.r.morgan@gmail.com>
2017-10-27 17:10:34 +0100

    add bodyParser code for app.use

--- server.js
+++ server.js
@@ -6,8 +6,22 @@ var bodyParser = require('body-parser');
 var routes = require('./app/routes/index.js');
 var api = require('./app/api/timestamp.js');
 
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET")
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use('/public', express.static(process.cwd() + '/public'));
+
 var port = process.env.PORT || 8080;
+
+routes(app);
+api(app);
 
 app.listen(port, function() {
   console.log('The server is listening on port ' + port);
-})
+});
